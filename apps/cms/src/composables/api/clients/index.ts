8ab2419b08788c7a9732d1a3ca6ad9f7fec8b32c
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2025-05-18 16:06:15 +0800

    feat(cms): 升级 alova 实例配置，集成 Vue Hook 状态管理并优化请求拦截器

--- apps/cms/src/composables/api/clients/index.ts
+++ apps/cms/src/composables/api/clients/index.ts
@@ -1,21 +1,23 @@
-import { createAlova } from 'alova';
-import fetchAdapter from 'alova/fetch';
-import { createApis, withConfigType } from './createApis';
+import { createAlova } from 'alova'
+import fetchAdapter from 'alova/fetch'
+import VueHook from 'alova/vue'
+import { createApis, withConfigType } from './createApis'
 
 export const alovaInstance = createAlova({
+  statesHook: VueHook,
   baseURL: '',
   requestAdapter: fetchAdapter(),
-  beforeRequest: method => {},
-  responded: res => {
-    return res.json();
-  }
-});
+  beforeRequest: (method) => { },
+  responded: (res) => {
+    return res.json()
+  },
+})
 
-export const $$userConfigMap = withConfigType({});
+export const $$userConfigMap = withConfigType({})
 
 /**
  * @type { EndApis }
  */
-const Apis = createApis(alovaInstance, $$userConfigMap);
+const Apis = createApis(alovaInstance, $$userConfigMap)
 
-export default Apis;
+export default Apis
