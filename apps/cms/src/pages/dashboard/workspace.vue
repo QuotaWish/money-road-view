02c7bf55bb1c6226cb188a5d4a21648e5e625ba0
mxr <1977368805@qq.com>
2025-05-21 10:57:08 +0800

    feat(cms): 优化页面布局并添加新功能
    - 在 components.d.ts 中添加了 ACol 和 ARow 组件的类型声明
    - 重构了 GaHeader 组件，增加了个人设置功能和模态框
    - 设计并实现了 dashboard 页面的布局，包括左侧主要内容区域和右侧快捷入口、最近访问等功能模块
    - 添加了 grid 布局样式，用于展示工作台的各个功能区块

--- apps/cms/src/pages/dashboard/workspace.vue
+++ apps/cms/src/pages/dashboard/workspace.vue
@@ -5,10 +5,99 @@ defineOptions({
 </script>
 
 <template>
-  <div>
-    Workspace
+  <div class="p-4 flex gap-4 h-full relative">
+    <div class="flex flex-1 flex-col" gap-4>
+      <div class="p-6" bg-red flex-1>
+        <P class="titleone">MONEYROAD</P>
+        <a-row class="grid-demo" justify="space-between">
+          <a-col :span="4">
+            <div>col - 4</div>
+          </a-col>
+          <a-col :span="4">
+            <div>col - 4</div>
+          </a-col>
+          <a-col :span="4">
+            <div>col - 4</div>
+          </a-col>
+          <a-col :span="4">
+            <div>col - 4</div>
+          </a-col>
+        </a-row>
+      </div>
+      <div class="flex w-full h-[40%] gap-6 justify-between">
+        <div class="p-4 flex-1" bg-red>2</div>
+        <div class="p-4 flex-1" bg-red>3</div>
+      </div>
+    </div>
+    <div class="flex flex-col gap-4 max-w-[300px] w-[30%]">
+      <div class="p-4 rounded-xl bg-red flex-1">
+        <p class="titleright">快捷入口</p>
+        <div class="layoutright">
+          <a-button type="primary" class="i-carbon:book">内容管理</a-button>
+          <a-button type="primary">内容统计</a-button>
+          <a-button type="primary">高级管理</a-button>
+          <a-button type="primary">线上推广</a-button>
+          <a-button type="primary">内容投放</a-button>
+        </div>
+      </div>
+      <div class="p-4 rounded-xl bg-red flex-1">
+        <p class="titleright">最近访问</p>
+        <div class="layoutright">
+          <a-button type="primary">数据统计</a-button>
+          <a-button type="primary">内容管理</a-button>
+          <a-button type="primary">高级管理</a-button>
+        </div>
+      </div>
+      <div class="p-4 rounded-xl bg-red flex-1">
+        公告
+      </div>
+      <div class="p-4 rounded-xl bg-red flex-1">
+        帮助文档
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.titleone {
+  font-size: 25px;
+}
+
+.titleright {
+  font-size: 20px;
+}
+
+.layoutright {
+  gap: 15px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+}
+
+.grid-demo {
+  background-color: var(--color-fill-2);
+  margin-bottom: 40px;
+}
+
+.grid-demo:last-child {
+  margin-bottom: 0px;
+}
+
+.grid-demo .arco-col {
+  height: 48px;
+  line-height: 48px;
+  color: var(--color-white);
+  text-align: center;
+}
+
+.grid-demo .arco-col:nth-child(2n) {
+  background-color: rgba(var(--arcoblue-6), 0.9);
+}
+
+.grid-demo .arco-col:nth-child(2n + 1) {
+  background-color: var(--color-primary-light-4);
+}
+</style>
 
 <route lang="yaml">
   meta:
