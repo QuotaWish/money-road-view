cdfcd61e309797dc680588238448bbd350cdc742
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2025-05-21 23:29:10 +0800

    feat(apps/ends): 添加配置模块
    在 app.module.ts 中引入了 ConfigurationModule，以支持应用程序的配置管理。

--- apps/ends/src/app.module.ts
+++ apps/ends/src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { jwtAuthGuard } from './guard/jwt-auth.guard';
 import { ConfigModule } from '@nestjs/config';
+import { ConfigModule as ConfigurationModule } from './modules/config/config.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, AuthModule],
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule, ConfigurationModule],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_GUARD,
